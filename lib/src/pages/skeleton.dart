0feb3cad9c9097cf2898facb3a2298942d7b2d85
Alejandro Monroy <>
2020-03-15 13:38:51 +0100

    main menu

--- lib/src/pages/skeleton.dart
+++ lib/src/pages/skeleton.dart
@@ -1,11 +1,81 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 
-class Skeleton extends StatelessWidget {
+class Skeleton extends StatefulWidget {
+  @override
+  _SkeletonState createState() => _SkeletonState();
+}
+
+class _SkeletonState extends State<Skeleton> {
+  int _currentIndex = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('SKELETON'),
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          children: <Widget>[
+            Container(
+              color: Colors.blueGrey,
+            ),
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.green,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        backgroundColor: Colors.black,
+        selectedIndex: _currentIndex,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            activeColor: Colors.white,
+            title: Text('Item One'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavyBarItem(
+            activeColor: Colors.white,
+            title: Text('Item One'),
+            icon: Icon(Icons.apps),
+          ),
+          BottomNavyBarItem(
+            activeColor: Colors.white,
+            title: Text('Item One'),
+            icon: Icon(Icons.chat_bubble),
+          ),
+          BottomNavyBarItem(
+            activeColor: Colors.white,
+            title: Text('Item One'),
+            icon: Icon(Icons.settings),
+          ),
+        ],
       ),
     );
   }
