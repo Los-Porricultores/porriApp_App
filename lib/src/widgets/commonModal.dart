1f033d1609e6697a34b44160ccc2927ffd1d1f09
Alejandro Monroy <>
2020-03-16 14:27:38 +0100

    no message

--- lib/src/widgets/commonModal.dart
+++ lib/src/widgets/commonModal.dart
@@ -1,13 +1,66 @@
 import 'package:flutter/material.dart';
 
 class CommonModal extends StatelessWidget {
+  final String modalTitle,
+      modalMessage,
+      modalNegativeButton,
+      modalPositiveButton;
+
+  CommonModal(
+    this.modalTitle,
+    this.modalMessage,
+    this.modalNegativeButton,
+    this.modalPositiveButton,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.transparent,
-      child: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[],
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(35),
+          topRight: Radius.circular(35),
+        ),
+      ),
+      child: Wrap(
+        children: <Widget>[
+          ListTile(
+            title: new Text(
+              modalTitle,
+              softWrap: true,
+              textAlign: TextAlign.center,
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            title: new Text(
+              modalMessage,
+              softWrap: true,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Chip(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  backgroundColor: Colors.redAccent,
+                  label: Text(modalNegativeButton),
+                ),
+                Chip(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  backgroundColor: Colors.lightGreen,
+                  label: Text(modalPositiveButton),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/src/widgets/rescueButton.dart
+++ lib/src/widgets/rescueButton.dart
@@ -1,70 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:porri_app/src/widgets/commonModal.dart';
 
 class RescueButton extends StatelessWidget {
+  final String modalTitle = 'Porri Rescate',
+      modalMessage =
+          'Al activar esta alerta se informará a todos los porricultores de tu estado de emergencia junto con tu posición.\n¿Estás seguro?',
+      modalNegativeButton = 'No, cancelar rescate',
+      modalPositiveButton = 'Sí, estoy seguro',
+      buttonText = 'SOS';
+
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              backgroundColor: Colors.transparent,
-              context: context,
-              builder: (BuildContext bc) {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(35),
-                      topRight: Radius.circular(35),
-                    ),
-                  ),
-                  child: Wrap(
-                    children: <Widget>[
-                      ListTile(
-                        title: new Text(
-                          'Porri Rescate',
-                          softWrap: true,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: Colors.black, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      ListTile(
-                        title: new Text(
-                          'Al activar esta alerta se informará a todos los porricultores de tu estado de emergencia junto con tu posición\n¿Estás seguro?',
-                          softWrap: true,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ),
-                      Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            Chip(
-                              padding: EdgeInsets.symmetric(horizontal: 10),
-                              backgroundColor: Colors.redAccent,
-                              label: Text('No, cancelar rescate'),
-                            ),
-                            Chip(
-                              padding: EdgeInsets.symmetric(horizontal: 10),
-                              backgroundColor: Colors.lightGreen,
-                              label: Text('Sí, estoy seguro'),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              });
-        },
-        backgroundColor: Colors.redAccent,
-        child: Text(
-          'SOS',
-          style: TextStyle(color: Colors.white),
-        ));
+      onPressed: () {
+        showModalBottomSheet(
+          backgroundColor: Colors.transparent,
+          context: context,
+          builder: (BuildContext buildContext) {
+            return CommonModal(modalTitle, modalMessage, modalNegativeButton,
+                modalPositiveButton);
+          },
+        );
+      },
+      backgroundColor: Colors.redAccent,
+      child: Text(
+        buttonText,
+        style: TextStyle(color: Colors.white),
+      ),
+    );
   }
 }
